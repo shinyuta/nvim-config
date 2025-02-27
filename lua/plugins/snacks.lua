f2b8e669470de82e8f9d9602347c144ab437ce2f
shinyuta <shinyoota@gmail.com>
2025-02-26 19:26:36 -0600

    Updated Noice and Notify, Added Useful Snacks Keybinds

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,28 +1,30 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    bigfile = { enabled = false },
-    dashboard = {
-      enabled = true,
-      preset = {
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          {
-            icon = " ",
-            key = "g",
-            desc = "Find Text",
-            action = ":lua Snacks.dashboard.pick('live_grep')",
-          },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-        header = [[
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = false },
+
+		-- DASHBOARD
+		dashboard = {
+			enabled = true,
+			preset = {
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+				header = [[
 
 
 
@@ -32,67 +34,82 @@ return {
 ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
 ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-      },
-      sections = {
-        { section = "header" },
+			},
+			sections = {
+				{ section = "header" },
 
-        { section = "startup", gap = 0, padding = 2, pane = 1 }, -- Ensures same gap as Git Status
+				{ section = "startup", gap = 0, padding = 2, pane = 1 }, -- Ensures same gap as Git Status
 
-        -- Keys Section with Controlled Gap
-        { section = "keys",    gap = 1, padding = 3 },
+				-- Keys Section with Controlled Gap
+				{ section = "keys", gap = 1, padding = 2 },
 
-        {
-          pane = 1,
-          icon = " ",
-          desc = "Browse Repo",
-          padding = 1,
-          key = "b",
-          action = function()
-            Snacks.gitbrowse()
-          end,
-        },
+				{
+					pane = 1,
+					icon = " ",
+					desc = "Browse Repo",
+					padding = 1,
+					key = "b",
+					action = function()
+						Snacks.gitbrowse()
+					end,
+				},
 
-        function()
-          local in_git = Snacks.git.get_root() ~= nil
-          local cmds = {
-            {
-              icon = " ",
-              title = "Git Status",
-              cmd = "git --no-pager diff --stat -B -M -C",
-              height = 10,
-            },
-          }
-          return vim.tbl_map(function(cmd)
-            return vim.tbl_extend("force", {
-              section = "terminal",
-              enabled = in_git,
-              padding = 1, -- Reduce extra space
-              gap = 1, -- Add small gap under Git Status (same as keys)
-              ttl = 5 * 60,
-              indent = 3,
-            }, cmd)
-          end, cmds)
-        end,
+				function()
+					local in_git = Snacks.git.get_root() ~= nil
+					local cmds = {
+						{
+							icon = " ",
+							title = "Git Status",
+							cmd = "git --no-pager diff --stat -B -M -C",
+							height = 10,
+						},
+					}
+					return vim.tbl_map(function(cmd)
+						return vim.tbl_extend("force", {
+							section = "terminal",
+							enabled = in_git,
+							padding = 1, -- Reduce extra space
+							gap = 1, -- Add small gap under Git Status (same as keys)
+							ttl = 5 * 60,
+							indent = 3,
+						}, cmd)
+					end, cmds)
+				end,
 
-        {
-          section = "terminal",
-          cmd = "echo -e '\n' && ascii-image-converter ~/.config/nvim/jinxdash.png --color --complex",
-          random = 10,
-          pane = 2,
-          indent = 4,
-          height = 30,
-        },
-      },
-    },
-    explorer = { enabled = false },
-    indent = { enabled = false },
-    input = { enabled = false },
-    picker = { enabled = false },
-    notifier = { enabled = false },
-    quickfile = { enabled = false },
-    scope = { enabled = false },
-    scroll = { enabled = false },
-    statuscolumn = { enabled = false },
-    words = { enabled = false },
-  },
+				{
+					section = "terminal",
+					cmd = "echo -e '\n' && ascii-image-converter ~/.config/nvim/jinxdash.png --color --complex",
+					random = 10,
+					pane = 2,
+					indent = 4,
+					height = 30,
+				},
+			},
+		},
+
+		-- NOTIFY
+		notifier = {
+			enabled = true,
+			timeout = 2000, -- default timeout in ms
+			width = { min = 40, max = 0.6 },
+			height = { min = 1, max = 0.6 },
+			style = "compact",
+		},
+		explorer = { enabled = false },
+		indent = { enabled = false },
+		input = { enabled = false },
+		picker = { enabled = false },
+		quickfile = { enabled = false },
+		scope = { enabled = false },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+
+		-- STYLES
+		styles = {
+			notification = {
+				wo = { wrap = true }, -- Wrap notifications
+			},
+		},
+	},
 }
