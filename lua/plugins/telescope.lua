81bcebcc801d4e142e2fc13894f9617946c18995
shinyuta <shinyoota@gmail.com>
2025-03-02 18:37:25 -0600

    Smarter telescope (for big files)
    as above.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,40 @@ return {
 		tag = "0.1.6",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			local previewers = require("telescope.previewers")
+
+			-- Custom previewer to skip large files and binaries
+			local new_maker = function(filepath, bufnr, opts)
+				opts = opts or {}
+
+				-- Check file size
+				local stat = vim.loop.fs_stat(filepath)
+				if stat and stat.size > 100 * 1024 then
+					-- File is too large for preview
+					return vim.schedule(function()
+						vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "⚠️ File too large to preview" })
+					end)
+				end
+
+				-- Default preview behavior for everything else
+				previewers.buffer_previewer_maker(filepath, bufnr, opts)
+			end
+
 			require("telescope").setup({
+				defaults = {
+					buffer_previewer_maker = new_maker,
+					file_ignore_patterns = {
+						"node_modules/.*",
+						".git/.*",
+						"%.jpg",
+						"%.jpeg",
+						"%.png",
+						"%.svg",
+						"%.gif",
+						"%.pdf",
+						"%.zip",
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
@@ -14,7 +47,7 @@ return {
 			})
 			require("telescope").load_extension("ui-select")
 
-			-- Optional aesthetic tweaks
+			-- Optional aesthetic tweaks (kept from your config)
 			local TelescopePrompt = {
 				TelescopePromptTitle = { fg = "#A9DD48", bg = "none" },
 				TelescopePreviewTitle = { fg = "#A9DD48", bg = "none" },
@@ -30,6 +63,6 @@ return {
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
-		-- Removed this entirely because it is now covered in the main telescope.nvim config
+		-- This is covered in the main telescope config, but can be left here if you want explicit control
 	},
 }
